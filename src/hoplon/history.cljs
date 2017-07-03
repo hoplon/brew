87c254365fc2e1dba477876a04120fe2c5ba00ca
Matthew Ratzke <matt@degree9.io>
2017-07-02 22:17:30 -0600

    Refactor History Cell (#27)
    * fbsync-cell
    
    * refactor history cell

--- src/hoplon/history.cljs
+++ src/hoplon/history.cljs
@@ -1,11 +1,28 @@
-(ns hoplon.history)
+(ns hoplon.history
+  (:require [goog.events :as events]
+            [javelin.core :as j])
+  (:import [goog History Uri]
+           [goog.history Html5History EventType]))
+
+(defn history [opts]
+  (if (Html5History.isSupported)
+    (doto (Html5History.)
+      (.setPathPrefix  (:prefix   opts ""))
+      (.setUseFragment (:fragment opts true)))
+    (History.)))
 
 (defn history-cell
- "A cell analagous to hoplon.core/route-cell, using Google Closure History API."
- []
- (let [c (j/cell nil)
-       history (History.)]
-  (j/with-let [_ (j/cell= c #(.setToken history %))]
-   (goog.events/listen history goog.History/EventType.NAVIGATE
-    (fn [e] (reset! c (.-token e))))
+ "A cell analagous to hoplon.core/route-cell, using Google Closure History API.
+
+  Optionally supports `:prefix` which will set an Html5History prefix, default `""`.
+
+  Optionally supports `:fragment` which will include page fragments in HTML5 browsers, default `true`.
+  This keeps routing backwards compatible with older browsers,
+ "
+ [& [{:keys [prefix fragment] :as opts}]]
+ (let [history  (history opts)
+       historyc (j/cell (.getToken history))]
+  (j/with-let [_ (j/cell= historyc (fn [token] (.setToken history token)))]
+   (events/listen history EventType.NAVIGATE
+    (fn [event] (.log js/console event) (reset! historyc (.-token event))))
    (.setEnabled history true))))
