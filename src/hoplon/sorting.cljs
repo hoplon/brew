048d899312d2976b65d470678d0b5b97b78032f6
Matthew Ratzke <matt@degree9.io>
2017-07-05 13:35:51 -0600

    update sorting.cljs

--- src/hoplon/sorting.cljs
+++ src/hoplon/sorting.cljs
@@ -5,16 +5,16 @@
   (->> data (map-indexed vector) (into {})))
 
 (defn indexed-cell
-  "Returns a formula cell which sorts and indexes a collection."
+  "Returns a formula cell which indexes a collection."
   [data] (j/cell= (index-coll data)))
 
 (defn sort-indexed [data]
   (-> data (sort) (index-coll)))
 
 (defn sort-by-indexed [data sort]
-  (-> data (sort-by sort) (index-coll)))
+  (->> data (sort-by sort) (index-coll)))
 
 (defn sorted-cell
   "Returns a formula cell which sorts and indexes a collection."
   ([data] (j/cell= (sort-indexed data)))
-  ([data sort] (j/cell= (sort-by-indexed data sort))))
+  ([sort data] (j/cell= (sort-by-indexed data sort))))
