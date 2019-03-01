782f2842df45493bdb4507fe578400b85c553da9
Matthew Ratzke <matt@degree9.io>
2019-02-28 20:16:25 -0700

    Rebase

--- src/hoplon/skel.cljs
+++ src/hoplon/skel.cljs
@@ -1,6 +1,5 @@
 (ns hoplon.skel
-  (:require [clojure.string :as s]
-            [javelin.core :as j]
+  (:require [javelin.core :as j]
             [cljsjs.skel]
             [goog.object :as obj])
   (:require-macros [hoplon.skel :refer [with-breakpoint]]))
@@ -13,11 +12,9 @@
 (defn breakpoints!
   ([]
    (breakpoints! :xlarge "1680px" :large  "1280px" :medium "980px" :small  "736px" :xsmall "480px"))
-  ([& breakpoints]
-    (->> breakpoints
-      (into {})
-      (map #(identity [(key %) (s/format "(max-width:%s)" (val %))]))
-      (into {})
+  ([& {:keys [xlarge large medium small xsmall]}]
+    (->> {:xlarge xlarge :large large :medium medium :small small :xsmall xsmall}
+      (reduce-kv #(assoc %1 %2 (str "(max-width:" %3 ")")) {})
       (clj->js)
       (.breakpoints js/skel))))
 
