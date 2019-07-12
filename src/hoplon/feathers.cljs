a3657a85bdf336b5aba397d5fa803de6ba6d2280
Matthew Ratzke <matt@degree9.io>
2019-07-11 19:36:01 -0600

    Update feathers.cljs

--- src/hoplon/feathers.cljs
+++ src/hoplon/feathers.cljs
@@ -2,7 +2,24 @@
   (:require [javelin.core :as j]
             [feathers.client.services :as fs]))
 
-(defn find-cell [app service & params]
+(defn feathers-created [fcell]
+  (fn [data] (swap! fcell conj (js->clj data :keywordize-keys true))))
+
+(defn remap [coll index]
+  (zipmap (map index coll) coll))
+
+(defn compare-index [index coll1 coll2]
+  (= (get coll1 index)
+     (get coll2 index)))
+
+;(defn feathers-updated [fcell]
+;  (fn [data]
+;    (let [data (js->clj data :keywordize-keys true)]
+;      (swap! fcell
+;        (fn [fdata]
+;          (conj (remove (partial compare-index :_id data) fdata) data))))
+
+(defn feathers-cell [app service & params]
   (let [fcell   (j/cell nil)
         fcell!  #(reset! fcell %)
         error!  #(.error js/console %)
@@ -12,7 +29,7 @@
         (fs/find (clj->js params))
         (.then fcell!)
         (.catch error!))
-      (fs/created service #(prn "created" %))
+      (fs/created service (feathers-created fcell))
       (fs/updated service #(prn "updated" %))
       (fs/patched service #(prn "patched" %))
       (fs/removed service #(prn "removed" %)))))
