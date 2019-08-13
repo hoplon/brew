0428674eec91c5f7c748ea7730b7141b3c858d6e
Matthew Ratzke <matt@degree9.io>
2019-08-12 18:55:54 -0600

    Update feathers.cljs

--- src/hoplon/feathers.cljs
+++ src/hoplon/feathers.cljs
@@ -5,32 +5,44 @@
 (defn feathers-created [fcell]
   (fn [data] (swap! fcell conj (js->clj data :keywordize-keys true))))
 
-(defn remap [coll index]
+(defn- remap [coll index]
   (zipmap (map index coll) coll))
 
-(defn compare-index [index coll1 coll2]
-  (= (get coll1 index)
-     (get coll2 index)))
+(defn feathers-updated [fcell]
+  (fn [data]
+    (let [data (js->clj data :keywordize-keys true)]
+      (swap! fcell
+        (fn [fdata]
+          (let [fdata (remap fdata :_id)]
+            (vals (assoc fdata (:_id data) data))))))))
 
-;(defn feathers-updated [fcell]
-;  (fn [data]
-;    (let [data (js->clj data :keywordize-keys true)]
-;      (swap! fcell
-;        (fn [fdata]
-;          (conj (remove (partial compare-index :_id data) fdata) data))))
+(defn feathers-patched [fcell]
+  (fn [data]
+    (let [data (js->clj data :keywordize-keys true)]
+      (swap! fcell
+        (fn [fdata]
+          (let [fdata (remap fdata :_id)]
+            (vals (assoc fdata (:_id data) (merge (get fdata (:_id data)) data)))))))))
+
+(defn feathers-removed [fcell]
+  (fn [data]
+    (let [data (js->clj data :keywordize-keys true)]
+      (swap! fcell
+        (fn [fdata]
+          (remove #(= (:_id %) (:_id data)) fdata))))))
+
 
 (defn feathers-cell [service & params]
   (let [fcell   (j/cell nil)
         fcell!  #(reset! fcell %)
         error!  #(.error js/console %)]
-    (j/cell= (prn "feathers-cell" fcell))
     (j/with-let [_ (j/cell= fcell fcell!)]
       (-> service
         (fs/find (clj->js params))
-        (.then js->clj)
+        (.then #(js->clj % :keywordize-keys true))
         (.then fcell!)
         (.catch error!))
       (fs/created service (feathers-created fcell))
-      (fs/updated service #(prn "updated" %))
+      (fs/updated service (feathers-updated fcell))
       (fs/patched service #(prn "patched" %))
-      (fs/removed service #(prn "removed" %)))))
+      (fs/removed service (feathers-removed fcell)))))
