df387ea2c232544c5852eba5f9247c27a52d5f94
Matthew Ratzke <matt@degree9.io>
2019-11-11 18:27:51 -0700

    add hoplon debug to feathers

--- src/hoplon/feathers.cljs
+++ src/hoplon/feathers.cljs
@@ -1,15 +1,21 @@
 (ns hoplon.feathers
   (:require [javelin.core :as j]
+            [hoplon.debug :as debug]
             [feathers.client.services :as fs]))
 
+(def ^:private debug (debug/debug :hoplon/feathers))
+
 (defn feathers-created [fcell]
-  (fn [data] (swap! fcell conj (js->clj data :keywordize-keys true))))
+  (fn [data]
+    (debug "feathers-created called with data" data)
+    (swap! fcell conj (js->clj data :keywordize-keys true))))
 
 (defn- remap [coll index]
   (zipmap (map index coll) coll))
 
 (defn feathers-updated [fcell]
   (fn [data]
+    (debug "feathers-updated called with data" data)
     (let [data (js->clj data :keywordize-keys true)]
       (swap! fcell
         (fn [fdata]
@@ -18,6 +24,7 @@
 
 (defn feathers-patched [fcell]
   (fn [data]
+    (debug "feathers-patched called with data" data)
     (let [data (js->clj data :keywordize-keys true)]
       (swap! fcell
         (fn [fdata]
@@ -26,23 +33,25 @@
 
 (defn feathers-removed [fcell]
   (fn [data]
+    (debug "feathers-removed called with data" data)
     (let [data (js->clj data :keywordize-keys true)]
       (swap! fcell
         (fn [fdata]
           (remove #(= (:_id %) (:_id data)) fdata))))))
 
+(defn- feathers-error! [error]
+  (throw error))
 
 (defn feathers-cell [service & params]
   (let [fcell   (j/cell nil)
-        fcell!  #(reset! fcell %)
-        error!  #(.error js/console %)]
+        fcell!  (partial reset! fcell)]
     (j/with-let [_ (j/cell= fcell fcell!)]
       (-> service
         (fs/find (clj->js params))
         (.then #(js->clj % :keywordize-keys true))
         (.then fcell!)
-        (.catch error!))
+        (.catch feathers-error!))
       (fs/created service (feathers-created fcell))
       (fs/updated service (feathers-updated fcell))
-      (fs/patched service #(prn "patched" %))
+      (fs/patched service (feathers-patched fcell))
       (fs/removed service (feathers-removed fcell)))))
